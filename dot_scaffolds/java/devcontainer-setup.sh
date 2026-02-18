54227694a69c644a9a16cd10a8c2b9c7b938f7c7
EdimarDeSa <edimar.sa@mngs.com.br>
2026-02-18 13:06:03 +0000

    Limpeza de coisas do scafold usado

--- dot_scaffolds/java/devcontainer-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export DEV_CONTAINER=1
-export GITHUB_USERNAME="EdimarMngs"
-
-sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
-
-echo "export DEV_CONTAINER=1" >> ~/.config/.exports
--- dot_scaffolds/python/devcontainer-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export DEV_CONTAINER=1
-chezmoi init --apply git@github.com:EdimarMngs/dotfiles.git
-
-echo "export DEV_CONTAINER=1" >>~/.zshrc
-
-uv lock
-uv sync --all-groups
